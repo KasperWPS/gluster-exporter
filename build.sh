f6c1f661fb71c35a20ed793ba0bc3b4fa143f531
Ivan Ivanov <kasper_wps@mail.ru>
2024-05-13 01:25:38 +0300

    Add comment

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+#sudo dnf install golang rpmdevtools
+
 rpmdev-setuptree
 
 cd gluster-exporter
+# Download project dependencies
+go mod tidy
+
 git archive --format=tar.gz --prefix=gluster-exporter-1.0/ -o gluster-exporter-1.0.tar.gz HEAD
 
 mv gluster-exporter-1.0.tar.gz ~/rpmbuild/SOURCES
